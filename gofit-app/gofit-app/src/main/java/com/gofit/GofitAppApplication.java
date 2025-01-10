cfdc859e02ae8c8328541c71171b469ff7b91806
spham52 <pcmrsteven@gmail.com>
2025-01-10 20:48:13 +1100

    Refreshing .gitignore rules

--- gofit-app/gofit-app/src/main/java/com/gofit/GofitAppApplication.java
+++ gofit-app/gofit-app/src/main/java/com/gofit/GofitAppApplication.java
@@ -1,13 +1,23 @@
 package com.gofit;
 
+import com.gofit.userDAO.UserDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GofitAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GofitAppApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(UserDAO userDAO) {
+		return runner -> {
+
+		};
 	}
 
 }
