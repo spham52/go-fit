9f845bef40044aaeeb7173231e78dead453a3f22
spham52 <pcmrsteven@gmail.com>
2025-01-11 20:32:15 +1100

    Implement ActivitiesService class

---
+++ gofit-app/gofit-app/src/main/java/com/gofit/service/ActivitiesService.java
@@ -0,0 +1,25 @@
+package com.gofit.service;
+
+import com.gofit.dao.ActivitiesDAO;
+import com.gofit.entity.Activities;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface ActivitiesService {
+
+    List<Activities> getAllActivities();
+
+    Activities getActivities(int id);
+
+    Activities save(Activities activities);
+
+    Activities update(Activities activities);
+
+    void delete(int id);
+
+    Activities addToUser(Activities activities, int userID);
+
+    List<Activities> getAllFromUser(int userID);
+}
---
+++ gofit-app/gofit-app/src/main/java/com/gofit/service/ActivitiesServiceImpl.java
@@ -0,0 +1,81 @@
+package com.gofit.service;
+
+import com.gofit.dao.ActivitiesDAO;
+import com.gofit.entity.Activities;
+import com.gofit.entity.User;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ActivitiesServiceImpl implements ActivitiesService {
+
+    private ActivitiesDAO activitiesDAO;
+    private UserService userService;
+
+    @Autowired
+    public ActivitiesServiceImpl(ActivitiesDAO activitiesDAO, UserService userService) {
+        this.activitiesDAO = activitiesDAO;
+        this.userService = userService;
+    }
+
+    @Override
+    public List<Activities> getAllActivities() {
+        return activitiesDAO.getAll();
+    }
+
+    @Override
+    public Activities getActivities(int id) {
+        return activitiesDAO.get(id);
+    }
+
+    @Override
+    @Transactional
+    public Activities save(Activities activities) {
+        return activitiesDAO.save(activities);
+    }
+
+    @Override
+    @Transactional
+    public Activities update(Activities activities) {
+        return activitiesDAO.update(activities);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+        Activities activities = activitiesDAO.get(id);
+        if (activities.isCustom()) {
+            User user = activities.getUser();
+            user.getActivities().remove(activities);
+            activities.setUser(null);
+        }
+
+        activitiesDAO.delete(id);
+    }
+
+    @Override
+    @Transactional
+    public Activities addToUser(Activities activities, int userID) {
+        User user = userService.findByID(userID);
+
+        if (user.getActivities() == null) {
+            user.setActivities(new ArrayList<>());
+        }
+
+        // activity is a custom activity added by the user
+        activities.setCustom(true);
+        user.getActivities().add(activities);
+        activities.setUser(user);
+        return activitiesDAO.save(activities);
+    }
+
+    @Override
+    public List<Activities> getAllFromUser(int userID) {
+        return activitiesDAO.getAllFromUser(userID);
+    }
+
+}
