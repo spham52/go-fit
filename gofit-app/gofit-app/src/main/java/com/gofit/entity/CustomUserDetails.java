8f0229a7587d0230a03b286ed6403fa64c9edf9f
Steven <pcmrsteven@gmail.com>
2025-01-18 17:40:22 +1100

    Add CustomUserDetails class

---
+++ gofit-app/gofit-app/src/main/java/com/gofit/entity/CustomUserDetails.java
@@ -0,0 +1,30 @@
+package com.gofit.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
