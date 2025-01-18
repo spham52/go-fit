cfe71a797bef4dbff38d2e074e4f505d002b92fe
Steven <pcmrsteven@gmail.com>
2025-01-18 17:49:40 +1100

    Create UserDetailsService class

--- gofit-app/gofit-app/src/main/java/com/gofit/service/UserDetailsServiceImpl.java
+++ gofit-app/gofit-app/src/main/java/com/gofit/service/UserDetailsServiceImpl.java
@@ -2,17 +2,17 @@ package com.gofit.service;
 
 import com.gofit.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
     private UserService userService;
 
     @Autowired
-    public CustomUserDetailsService(UserService userService) {
+    public UserDetailsServiceImpl(UserService userService) {
         this.userService = userService;
     }
 
@@ -20,11 +20,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.findByUsername(username);
         if (user == null) {
-            throw new UsernameNotFoundException("User: " + username + " not found");
+            throw new UsernameNotFoundException(username);
         }
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getUsername())
                 .password(user.getPassword())
+                .authorities(user.getRoles())
                 .build();
     }
 }
