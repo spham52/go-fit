192a68f226fb1f75a15459c9415bdb35a30a4d2d
Steven <pcmrsteven@gmail.com>
2025-01-24 13:24:46 +1100

    Add delegatingPasswordEncoder

--- gofit-app/gofit-app/src/main/java/com/gofit/configuration/AppConfig.java
+++ gofit-app/gofit-app/src/main/java/com/gofit/configuration/AppConfig.java
@@ -1,15 +1,20 @@
 package com.gofit.configuration;
 
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
-
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
