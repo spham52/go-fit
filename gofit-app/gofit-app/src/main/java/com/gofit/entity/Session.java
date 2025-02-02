a425ce9f32b8636e33481da3b170078ff7f4d1b0
Steven <pcmrsteven@gmail.com>
2025-02-02 22:43:06 +1100

    - Add JsonIgnore annotation - Add additional field for sessionName - Removed constructor parameter 'time'

--- gofit-app/gofit-app/src/main/java/com/gofit/entity/Session.java
+++ gofit-app/gofit-app/src/main/java/com/gofit/entity/Session.java
@@ -1,41 +1,58 @@
 package com.gofit.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="session")
+@Table(name = "session")
 public class Session {
 
     @NotNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="session_id")
+    @Column(name = "session_id")
     private int sessionID;
+
+    @NotBlank
+    @Column(name = "session_name")
+    private String sessionName;
 
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE,
-                                                  CascadeType.DETACH, CascadeType.REFRESH})
+            CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
-    @NotNull
-    @Column(name="log_time")
+    @Column(name = "log_time")
     private LocalDateTime time;
 
     // each session holds multiple activityLogs
     // activityLogs are exercises/workouts recorded by the user in a single session
-    @OneToMany(mappedBy = "session", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "session", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonIgnore
     List<ActivityLog> activityLog;
 
-    public Session() {}
+    public Session() {
+    }
 
-    public Session(User user, LocalDateTime time) {
+    public Session(String sessionName, User user) {
+        this.sessionName = sessionName;
         this.user = user;
-        this.time = time;
+    }
+
+    public String getSessionName() {
+        return sessionName;
+    }
+
+    public void setSessionName(String sessionName) {
+        this.sessionName = sessionName;
     }
 
     public int getSessionID() {
